97f47a6df95c33bf8836f4c1041d62d6dc93290b
ulya <149610916+fasolka12345@users.noreply.github.com>
2025-04-18 17:29:20 +0300

    Version 2.0

--- app/src/main/java/com/example/mygrafics/GraficsActivity.java
+++ app/src/main/java/com/example/mygrafics/GraficsActivity.java
@@ -1,6 +1,8 @@
 package com.example.mygrafics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,14 @@ public class GraficsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.grafics_main);
-    
+        setContentView(R.layout.activity_main);
+
+        View view = findViewById(R.id.textView2);
+        view.setOnClickListener(this::pushStart);
+    }
+
+    public void pushStart(View view) {
+        Intent intent = new Intent(getApplicationContext(), GraficsActivity.class);
+        startActivity(intent);
     }
 }
